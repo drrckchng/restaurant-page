1b3f99e6ce4e6feff43c714484f9e4c48168bd34
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-05 18:54:19 +0900

    Add title and motto and remove background image

--- src/home.js
+++ src/home.js
@@ -1,6 +1,24 @@
+import SplashImage from './splash.png';
+
 export default function loadHomePage() {
   const content = document.getElementById("content");
+
+  // Add splash banner
   const splash = document.createElement("div");
+  splash.classList.add("splash");
+
+  // Add resturant name
+  const restaurantName = document.createElement("p");
+  restaurantName.textContent = "Almamigo";
+  restaurantName.classList.add("title");
+  splash.appendChild(restaurantName);
+
+  // Add restaurant motto
+  const motto = document.createElement("p");
+  motto.textContent = "LA Soul in Seoul";
+  motto.classList.add("motto");
+  splash.appendChild(motto);
+
   content.appendChild(splash);
 
   // div.classList.add("splash-image");
