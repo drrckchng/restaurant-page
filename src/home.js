2669e4cf8b1c2d17882276ffe1a9a5770fd477b6
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-05 17:18:08 +0900

    Defer JS loading and test adding element through home page module

--- src/home.js
+++ src/home.js
@@ -1,5 +1,8 @@
 export default function loadHomePage() {
+  const content = document.getElementById("content");
   const splash = document.createElement("div");
+  content.appendChild(splash);
+
   // div.classList.add("splash-image");
   // Title/Banner with motto/saying
   // We deliver!
