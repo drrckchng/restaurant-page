02155f3a8b2fae1c4b582193b4feb42e58f1e4a3
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-10 02:15:22 +0900

    Flesh out button event listeners and clear page functions

--- src/topbar.js
+++ src/topbar.js
@@ -1,3 +1,8 @@
+import loadHomePage from './home.js';
+// import loadMenuPage from './menu.js';
+// import loadContactPage from './menu.js';
+// import clearPage from './clear.js'
+
 export default function loadTopBar() {
   // Grab initial content div
   const content = document.getElementById("content");
@@ -17,6 +22,22 @@ export default function loadTopBar() {
   menuButton.textContent = "Menu";
   const contactButton = document.createElement("button");
   contactButton.textContent = "Contact";
+
+  // Add event listener
+  homeButton.addEventListener("click", function() {
+    // clearPage()
+    loadHomePage();
+  });
+
+  menuButton.addEventListener("click", function() {
+    // clearPage()
+    // loadMenuPage();
+  });
+
+  contactButton.addEventListener("click", function() {
+    // clearPage()
+    // loadContactPage();
+  });
 
   topBar.appendChild(homeButton);
   topBar.appendChild(menuButton);
