7359c643e9c5fbe076549fac02528ee95fabf3b1
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-11 21:52:02 +0900

    Add images for food items

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,36 @@
 import loadTopBar from './topbar.js';
+import Tacos from './pictures/tacos.jpg';
+import Burrito from './pictures/burrito.jpg';
+import Quesadillas from './pictures/quesadilla.jpg';
 
+// Grab content div
+const content = document.getElementById("content");
+
+function addFoods() {
+  const foodSection = document.createElement("div");
+  const foods = document.createElement("p");
+  foods.textContent = "Food";
+  foodSection.appendChild(foods);
+
+  const tacoPicture = new Image();
+  const burritoPicture = new Image();
+  const quesadillasPicture = new Image();
+
+  // Food
+    // Tacos
+    // Burritos
+    // Quesadillas
+  content.appendChild(foodSection);
+}
+
+
+function addDrinks() {
+  // Drinks
+    // Jarritos
+    // Horchatas
+}
 export default function loadMenuPage() {
-
-  // Load top bar
   loadTopBar();
-
-  console.log("menu page");
+  addFoods();
+  addDrinks();
 }
