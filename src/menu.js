84d12826061c84a47d4eae5190818dc89a559d29
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-11 22:36:59 +0900

    Flesh out menu page

--- src/menu.js
+++ src/menu.js
@@ -2,33 +2,62 @@ import loadTopBar from './topbar.js';
 import Tacos from './pictures/tacos.jpg';
 import Burrito from './pictures/burrito.jpg';
 import Quesadillas from './pictures/quesadilla.jpg';
+import Jarritos from './pictures/jarritos.jpg';
+import Horchata from './pictures/horchata.jpg';
 
 // Grab content div
 const content = document.getElementById("content");
 
+function createSection(headerName, className) {
+  const div = document.createElement("div");
+  div.classList.add("section", className);
+  const header = document.createElement("p");
+  header.textContent = headerName;
+  div.appendChild(header);
+  content.appendChild(div);
+}
+
+function createFoodAndDrink(parent, img, description, className) {
+  const div = document.createElement("div");
+  div.classList.add("item-entry", className);
+  div.appendChild(img);
+  const itemDescription = document.createElement("p");
+  itemDescription.textContent = description;
+  div.appendChild(itemDescription);
+  parent.appendChild(div);
+}
+
 function addFoods() {
-  const foodSection = document.createElement("div");
-  const foods = document.createElement("p");
-  foods.textContent = "Food";
-  foodSection.appendChild(foods);
+  createSection("Food", "food-section");
+  const foodSection = document.getElementsByClassName("food-section")[0];
 
   const tacoPicture = new Image();
   const burritoPicture = new Image();
   const quesadillasPicture = new Image();
+  
+  tacoPicture.src = Tacos;
+  burritoPicture.src = Burrito;
+  quesadillasPicture.src = Quesadillas;
 
-  // Food
-    // Tacos
-    // Burritos
-    // Quesadillas
-  content.appendChild(foodSection);
+  createFoodAndDrink(foodSection, tacoPicture, "Pollo, Carnitas, Barbacoa, Tripas", "tacos");
+  createFoodAndDrink(foodSection, burritoPicture, "Pollo, Carnitas, Barbacoa", "burritos");
+  createFoodAndDrink(foodSection, quesadillasPicture, "Pollo, Carnitas, Barbacoa", "quesadillas");
 }
-
 
 function addDrinks() {
-  // Drinks
-    // Jarritos
-    // Horchatas
+  createSection("Drink", "drink-section");
+  const drinkSection = document.getElementsByClassName("drink-section")[0];
+
+  const jarritosPicture = new Image();
+  const horchataPicture = new Image();
+
+  jarritosPicture.src = Jarritos;
+  horchataPicture.src = Horchata;
+
+  createFoodAndDrink(drinkSection, jarritosPicture, "Traditional Mexican soda");
+  createFoodAndDrink(drinkSection, horchataPicture, "Traditional Mexican drink");
 }
+
 export default function loadMenuPage() {
   loadTopBar();
   addFoods();
