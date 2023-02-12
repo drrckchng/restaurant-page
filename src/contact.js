8f3f38af1f53bf05a09032d74743a669ee02d5b1
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-12 17:49:46 +0900

    Flesh out contact page

--- src/contact.js
+++ src/contact.js
@@ -1,9 +1,46 @@
 import loadTopBar from './topbar.js';
 
+// Grab content div
+const content = document.getElementById("content");
+
+function addPhoneNumber() {
+  const phoneNumbers = document.createElement("div");
+  phoneNumbers.classList.add("contact-phone");
+  const header = document.createElement("h1");
+  header.textContent = "Phone";
+  phoneNumbers.appendChild(header);
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "+82-02-213-206"
+  phoneNumbers.appendChild(phoneNumber);
+  content.appendChild(phoneNumbers);
+}
+
+function addSocialMedia() {
+  const socials = document.createElement("div");
+  socials.classList.add("contact-socials");
+  const header = document.createElement("h1");
+  header.textContent = "Social Media";
+  socials.appendChild(header);
+  const instagram = document.createElement("p");
+  instagram.textContent = "Instagram: @almamigo_official";
+  socials.appendChild(instagram);
+  content.appendChild(socials);
+}
+
+function addLocation() {
+  const location = document.createElement("div");
+  const header = document.createElement("h1");
+  header.textContent = "Location";
+  location.appendChild(header);
+  const address = document.createElement("p");
+  address.textContent = "Seoul Sinsa-dong 663-23"
+  location.appendChild(address);
+  content.appendChild(location);
+}
+
 export default function loadContactPage() {
-
-  // Load top bar
   loadTopBar();
-
-  console.log("contact page");
+  addPhoneNumber();
+  addSocialMedia();
+  addLocation();
 }
