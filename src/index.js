909aa78f4c520ce7ac84d2e5fa0f04b40949c195
drrckchng <19944575+drrckchng@users.noreply.github.com>
2023-02-15 15:14:57 +0900

    Add bold font

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 import loadTopBar from './topbar.js';
 import loadHomePage from './home.js';
 import "@fontsource/roboto";
+import "@fontsource/roboto/900.css";
 
 loadTopBar();
 loadHomePage();
